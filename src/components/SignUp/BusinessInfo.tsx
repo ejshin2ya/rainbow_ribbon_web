38de3cff6899dcf9f9c8437befd7a88a49e17e74
신은정 <ejshin0213@naver.com>
2024-08-24 09:08:19 +0900

    [FE]feat:주소 검색 기능 모달창 생성 및 사업자정보 컴포넌트에서 사용코드 추가

--- src/components/SignUp/BusinessInfo.tsx
+++ src/components/SignUp/BusinessInfo.tsx
@@ -2,30 +2,40 @@ import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import InputWithLabel from "../common/InputWithLabel";
 import Button from "../common/Button";
+import AddressModal from "../common/AddressModal";
+import Modal from "../common/Modal";
+import useModal from "../../hooks/useModal";
+import Input from "../common/Input";
+import { useRecoilState } from "recoil";
+import { signUpFormState } from "../../atoms/signupFormState";
 
 interface BusinessInfoProps {
   onSubmit: () => void;
-  onPrev: () => void;
-  updateFormData: (data: { businessInfo: BusinessInfoData }) => void;
 }
 
 interface BusinessInfoData {
   businessNumber: string;
   address: string;
+  zonecode: string;
+  detailedAdress: string;
   businessProof: File | null;
   licenseProof: File | null;
 }
 
-const BusinessInfo: React.FC<BusinessInfoProps> = ({
-  onSubmit,
-  updateFormData,
-}) => {
+const BusinessInfo: React.FC<BusinessInfoProps> = ({ onSubmit }) => {
+  //사업정보 입력값 데이터
   const [businessData, setBusinessData] = useState<BusinessInfoData>({
     businessNumber: "",
     address: "",
+    zonecode: "",
+    detailedAdress: "",
     businessProof: null,
     licenseProof: null,
   });
+  //recoil로 관리되는 회원가입시 요청 데이터
+  const [, setFormData] = useRecoilState(signUpFormState);
+  //모달창 hook
+  const { isOpen, openModal, closeModal } = useModal();
 
   // 숨겨진 file input에 대한 참조
   const businessProofInputRef = useRef<HTMLInputElement>(null);
@@ -35,8 +45,18 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
   const isFormValid =
     businessData.businessNumber &&
     businessData.address &&
+    businessData.zonecode &&
     businessData.businessProof &&
     businessData.licenseProof;
+
+  //사업자 등록번호 입력에 대한 유효성 검사 및 숫자만 입력 가능하도록 설정
+  const handleBusinessNumberChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    const numericValue = value.replace(/\D/g, "").slice(0, 10);
+    setBusinessData((prev) => ({ ...prev, [name]: numericValue }));
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -57,9 +77,31 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
     }
   };
 
+  // 주소 선택 시 businessData 상태를 업데이트하는 함수(AddressModal 컴포넌트로 전달)
+  const handleAddressComplete = (data: {
+    address: string;
+    zonecode: string;
+  }) => {
+    setBusinessData((prev) => ({
+      ...prev,
+      address: data.address,
+      zonecode: data.zonecode,
+    }));
+  };
+  //recoil로 관리되는 회원가입 요청 정보 데이터를 보내고, 제출함수를 불러와서 회원가입 요청을 진행
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    updateFormData({ businessInfo: businessData });
+    setFormData((prev) => ({
+      ...prev,
+      businessRegCertificateImage: businessData.businessProof,
+      animalBurialPermitImage: businessData.licenseProof,
+      companySignUpReq: {
+        ...prev.companySignUpReq,
+        businessRegNum: businessData.businessNumber,
+        address: businessData.address,
+        addressDetail: businessData.detailedAdress,
+      },
+    }));
     onSubmit();
   };
 
@@ -71,26 +113,64 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
         name="businessNumber"
         htmlFor="input-businessNumber"
         value={businessData.businessNumber}
-        onChange={handleChange}
+        onChange={handleBusinessNumberChange}
         placeholder="사업자 등록번호를 입력해주세요."
         required
       />
       <AddressContainer>
-        <InputWithLabel
-          label="주소"
-          htmlFor="input-address"
-          type="text"
-          name="address"
-          value={businessData.address}
-          onChange={handleChange}
-          placeholder="주소 찾기를 눌러주세요."
-          required
-        />
-        <Button addTopMargin={true}>주소 찾기</Button>
+        {businessData.zonecode ? (
+          <>
+            <InputBox>
+              <Input
+                type="text"
+                name="zonecode"
+                value={businessData.zonecode}
+                onChange={handleChange}
+                disabled
+              ></Input>
+              <Button onClick={openModal}>주소 찾기</Button>
+            </InputBox>
+            <InputWithLabel
+              label="주소"
+              htmlFor="input-address"
+              type="text"
+              name="address"
+              value={businessData.address}
+              onChange={handleChange}
+              placeholder="주소 찾기를 눌러주세요."
+              required
+              disabled
+            />
+            <Input
+              type="text"
+              name="detailedAdress"
+              value={businessData.detailedAdress}
+              onChange={handleChange}
+            ></Input>
+          </>
+        ) : (
+          <InputBox>
+            <InputWithLabel
+              label="주소"
+              htmlFor="input-address"
+              type="text"
+              name="address"
+              value={businessData.address}
+              onChange={handleChange}
+              placeholder="주소 찾기를 눌러주세요."
+              required
+              disabled
+            />
+            <Button addTopMargin={true} onClick={openModal}>
+              주소 찾기
+            </Button>
+          </InputBox>
+        )}
       </AddressContainer>
       <FileUpload>
         <label>사업자 등록증 첨부</label>
         <FileInputWrapper
+          hasFile={!!businessData.businessProof}
           onClick={() => triggerFileInput(businessProofInputRef)}
         >
           <input
@@ -101,15 +181,22 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
             ref={businessProofInputRef}
             required
           />
-          {businessData.businessProof
-            ? businessData.businessProof.name
-            : "이미지 추가"}
+          {businessData.businessProof && (
+            <PreviewImage
+              src={URL.createObjectURL(businessData.businessProof)}
+              alt="Preview"
+            />
+          )}
         </FileInputWrapper>
+        {businessData.businessProof && (
+          <FileName>{businessData.businessProof.name}</FileName>
+        )}
       </FileUpload>
 
       <FileUpload>
         <label>동물장묘 허가증 첨부</label>
         <FileInputWrapper
+          hasFile={!!businessData.licenseProof}
           onClick={() => triggerFileInput(licenseProofInputRef)}
         >
           <input
@@ -120,10 +207,16 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
             ref={licenseProofInputRef}
             required
           />
-          {businessData.licenseProof
-            ? businessData.licenseProof.name
-            : "이미지 추가"}
+          {businessData.licenseProof && (
+            <PreviewImage
+              src={URL.createObjectURL(businessData.licenseProof)}
+              alt="Preview"
+            />
+          )}
         </FileInputWrapper>
+        {businessData.licenseProof && (
+          <FileName>{businessData.licenseProof.name}</FileName>
+        )}
       </FileUpload>
       <Notice>
         내용을 충분히 확인할 수 있도록 깔끔하게 정렬된 이미지를 첨부해주세요.
@@ -136,6 +229,11 @@ const BusinessInfo: React.FC<BusinessInfoProps> = ({
           다음
         </Button>
       </ButtonGroup>
+      <AddressModal
+        isOpen={isOpen}
+        onClose={closeModal}
+        onComplete={handleAddressComplete}
+      />
     </Form>
   );
 };
@@ -148,25 +246,69 @@ const Form = styled.form`
 
 const AddressContainer = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 10px;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const FileUpload = styled.div`
   margin-top: 1rem;
 `;
 
-const FileInputWrapper = styled.div`
+const FileInputWrapper = styled.div<{ hasFile: boolean }>`
   display: flex;
   align-items: center;
-  margin-top: 0.5rem;
-  padding: 10px;
-  border: 1px dashed #ddd;
-  border-radius: 5px;
+  justify-content: center;
+  height: 150px;
+  background-color: #f9f9f9;
+  border: 2px dashed #ccc;
+  border-radius: 10px;
   cursor: pointer;
+  text-align: center;
+  color: #999;
+  font-size: 1rem;
+  position: relative;
 
   input[type="file"] {
     display: none;
   }
+
+  &::before {
+    content: "${(props) => (!props.hasFile ? "+" : "")}";
+    font-size: 2rem;
+    color: #999;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    display: ${(props) => (props.hasFile ? "none" : "block")};
+  }
+
+  &::after {
+    content: "${(props) => (!props.hasFile ? "이미지 추가" : "")}";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 10%);
+    color: #999;
+    display: ${(props) => (props.hasFile ? "none" : "block")};
+  }
+`;
+
+const PreviewImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 10px;
+`;
+
+const FileName = styled.span`
+  color: #333;
+  margin-top: 5px;
+  font-size: 0.875rem;
 `;
 
 const Notice = styled.p`
