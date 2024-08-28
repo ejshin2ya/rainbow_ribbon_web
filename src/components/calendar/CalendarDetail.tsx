28344bed0da05944502bf1be1048a9b9f7095379
HanKyeon <hgh21233@gmail.com>
2024-08-28 22:09:35 +0900

    feat: 불필요한 데이터 삭제 및 데이터 가공

--- src/components/calendar/CalendarDetail.tsx
+++ src/components/calendar/CalendarDetail.tsx
@@ -1,72 +1,85 @@
 import styled from 'styled-components';
-import { EventSourceInput } from '@fullcalendar/core';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-
-const events: EventSourceInput = [
-  {
-    title: '초롱이 (강아지) / 간단패키지',
-    start: '2024-08-25T10:00:00',
-    end: '2024-08-25T11:00:00',
-    status: '확정',
-  },
-  {
-    title: '나비 (고양이) / 기본패키지',
-    start: '2024-08-25T11:00:00',
-    end: '2024-08-25T12:00:00',
-    status: '확정',
-  },
-  {
-    title: '아롱이 (강아지) / 기본패키지',
-    start: '2024-08-25T13:00:00',
-    end: '2024-08-25T14:30:00',
-    status: '요청',
-  },
-  {
-    title: '샴1 (고양이) / 간단패키지',
-    start: '2024-08-25T22:00:00',
-    end: '2024-08-25T24:00:00',
-    status: '확정',
-  },
-  {
-    title: '샴2 (고양이) / 보통패키지',
-    start: '2024-08-25T22:00:00',
-    end: '2024-08-26T1:00:00',
-    status: '확정',
-  },
-  {
-    title: '으아악 (강아지) / 기본패키지',
-    start: '2024-08-25T22:00:00',
-    end: '2024-08-25T24:30:00',
-    status: '요청',
-  },
-];
+import { useEffect, useMemo, useState } from 'react';
+import { useFuneralEventStore } from './store/event-store';
 
 interface Props {
   selectedDate: Date;
 }
 
+export interface EventProps {
+  // TODO: maxWidth, width, height, top, right, left, startDate, endDate는 필수값으로 변경
+  maxWidth?: number | string;
+  width?: number | string;
+  height?: number;
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+  subTitle: string;
+  status: '확정' | '요청';
+  startDate: Date;
+  endDate: Date;
+}
+
+const EventItem = function ({
+  status,
+  subTitle,
+  bottom,
+  height,
+  left = 55,
+  maxWidth = '100%',
+  right = 0,
+  top = 0,
+  width,
+  startDate,
+  endDate,
+}: EventProps) {
+  // 이름 / 패키지 배경  폰트  왼쪽 보더
+  const boxColor =
+    status === '요청'
+      ? 'bg-reborn-yellow1 text-reborn-orange2_4 border-reborn-orange5'
+      : 'bg-reborn-blue0 border-l-reborn-blue1 text-reborn-blue2';
+  const buttonColor =
+    status === '요청'
+      ? 'bg-reborn-yellow2 text-reborn-orange2_4'
+      : 'bg-reborn-blue0_1 text-reborn-blue2';
+  const timeString = useMemo(() => {
+    const startHour = ('0' + startDate?.getHours()).slice(-2);
+    const startMinutes = ('0' + startDate?.getMinutes()).slice(-2);
+    const endHour = ('0' + endDate?.getHours()).slice(-2);
+    const endMinutes = ('0' + endDate?.getMinutes()).slice(-2);
+    return `${startHour}:${startMinutes} ~ ${endHour}:${endMinutes}`;
+  }, [startDate, endDate]);
+  return (
+    <div
+      className={`absolute min-h-[23px] z-[2] left-[55px] right-0 top-[23px] opacity-100 rounded-[4px] border-l-[3px] font-medium text-[12px] leading-[18px] cursor-pointer ${boxColor}`}
+      style={{
+        top,
+        left,
+        right,
+        bottom,
+        height,
+        width,
+        maxWidth,
+      }}
+    >
+      <div className="flex flex-row h-[23px] px-[3px] items-center">
+        <div className="flex-1">{timeString}</div>
+        <div
+          className={`rounded-[4px] px-[6px] py-[2px] w-[32px] h-[19px] text-[10px] font-medium leading-[18px] flex-shrink-0 text-center ${buttonColor}`}
+        >
+          {status}
+        </div>
+      </div>
+      <div className={`flex flex-row h-[23px] px-[3px] items-center`}>
+        {subTitle}
+      </div>
+    </div>
+  );
+};
+
 export const CalendarDetail = function ({ selectedDate }: Props) {
-  const dayParser = ['일', '월', '화', '수', '목', '금', '토'];
-  const cols = [
-    '오전 7시',
-    '오전 8시',
-    '오전 9시',
-    '오전 10시',
-    '오전 11시',
-    '오후 12시',
-    '오후 1시',
-    '오후 2시',
-    '오후 3시',
-    '오후 4시',
-    '오후 5시',
-    '오후 6시',
-    '오후 7시',
-    '오후 8시',
-    '오후 9시',
-    '오후 10시',
-    '오후 11시',
-    '오전 12시',
-  ];
+  const { processedEvents } = useFuneralEventStore();
 
   return (
     <CalendarContainer>
@@ -79,7 +92,7 @@ export const CalendarDetail = function ({ selectedDate }: Props) {
             {`${selectedDate.getDate()}일`}
           </h1>
           <h2 className="font-semibold text-[13px] leading-[20px] text-reborn-gray4 align-text-bottom">
-            {dayParser[selectedDate.getDay()]}
+            {['일', '월', '화', '수', '목', '금', '토'][selectedDate.getDay()]}
           </h2>
         </div>
         <div className="h-full flex flex-row gap-[12px]">
@@ -95,31 +108,56 @@ export const CalendarDetail = function ({ selectedDate }: Props) {
       </div>
 
       <div className="grid__container grid grid-rows-36 grid-cols-[55px_1fr]">
-        {/* <div className="top-0 left-[55px] right-0 bottom-0 bg-reborn-gray4 opacity-20">
-          여기가 이벤트 영역
-          <div className="absolute z-[2] left-0 right-0 top-[23px] opacity-100 text-reborn-gray8">
-            이벤트 들어갈 것임.
-          </div>
-        </div> */}
-        <div className="absolute min-h-[23px] z-[2] left-[55px] right-0 top-[23px] opacity-100 bg-reborn-blue0 rounded-[4px] border-l-reborn-blue1 border-l-[3px] text-reborn-blue2 font-medium text-[12px] leading-[18px]">
-          이벤트 들어갈 것임. 높이랑 top은 inline style로, width는 left right로
-          <div className="flex flex-row p-[3px]">
-            <div className="flex-1">이거 시간</div>
-            <div className="rounded-[4px] bg-reborn-blue0_1 text-reborn-blue2 px-[6px] py-[2px] w-[32px] h-[19px] text-[10px] font-medium leading-[18px] flex-shrink-0 text-center">
-              확정
-            </div>
-          </div>
-          <div className="flex flex-row p-[3px] ">
-            <div className="flex-1">
-              이름 / 패키지 배경 bg-reborn-yellow1 폰트 text-reborn-orange2_4
-              왼쪽 보더 border-reborn-orange5
-            </div>
-            <div className="rounded-[4px] bg-reborn-yellow2 text-reborn-orange2_4 px-[6px] py-[2px] w-[32px] h-[19px] text-[10px] font-medium leading-[18px] flex-shrink-0 text-center">
-              요청
-            </div>
-          </div>
-        </div>
-        {cols.map(title => {
+        {/* 이벤트 영역은 위에 */}
+        {/* 이벤트 들어갈 것임. 높이랑 top은 inline style로, width는 left right로 */}
+        {processedEvents.map((td, idx) => {
+          const diffDate = td.endDate.getTime() - td.startDate.getTime();
+          const diffHours = diffDate / (1000 * 60 * 60);
+          const height = diffHours * 46;
+          const top = (td.startDate.getHours() - 7) * 46;
+          const left = 55;
+          const width = `calc((100% - 55px) / ${td.overlapCount ?? 1})`;
+          console.log(td.subTitle, td.overlapCount, td.overlapIndex);
+          // if (idx > 0 && (td.dupCount ?? 1) > 1) {
+          //   let cnt = td.dupCount as number;
+          //   while (cnt) {}
+          // }
+
+          return (
+            <EventItem
+              key={`event-${idx}`}
+              status={td.status}
+              subTitle={td.subTitle}
+              startDate={td.startDate}
+              endDate={td.endDate}
+              height={height}
+              top={top}
+              left={left}
+              width={width}
+            />
+          );
+        })}
+
+        {[
+          '오전 7시',
+          '오전 8시',
+          '오전 9시',
+          '오전 10시',
+          '오전 11시',
+          '오후 12시',
+          '오후 1시',
+          '오후 2시',
+          '오후 3시',
+          '오후 4시',
+          '오후 5시',
+          '오후 6시',
+          '오후 7시',
+          '오후 8시',
+          '오후 9시',
+          '오후 10시',
+          '오후 11시',
+          '오전 12시',
+        ].map(title => {
           return (
             <>
               <div className="cell border-none pr-[8px] py-[4px]">{title}</div>
