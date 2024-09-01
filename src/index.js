1dc7168f4f259b55cd6b979a23f419e55febcb23
HanKyeon <hgh21233@gmail.com>
2024-09-01 15:10:13 +0900

    feat: add defaultOptions of queryClient

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 
 // QueryClient 인스턴스 생성
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 10 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+    },
+  },
+});
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     {/* QueryClientProvider로 전체 애플리케이션을 감쌉니다. */}
@@ -21,7 +31,7 @@ root.render(
       {/* ReactQueryDevtools는 QueryClientProvider 내부에 위치해야 합니다. */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
