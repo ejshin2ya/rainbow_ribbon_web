e2a9d798cd58f90be13b909584eb021ac2eae9ce
HanKyeon <hgh21233@gmail.com>
2024-09-01 17:47:04 +0900

    feat: commonRoute에 stomp Provider 추가

--- src/components/CommonRoute.tsx
+++ src/components/CommonRoute.tsx
@@ -3,20 +3,23 @@ import { AppBar } from './AppBar';
 import { SideBar } from './SideBar';
 import { ConfirmDialog } from './confirm-dialog/ConfitmDialog';
 import { ConfirmDialogProvider } from './confirm-dialog/confitm-dialog-store';
+import { StompProvider } from 'src/queries';
 
 export const CommonRoute = function () {
   return (
-    <ConfirmDialogProvider>
-      <ConfirmDialog />
-      <div className="w-full h-full flex flex-col bg-gradient-to-r from-reborn-gray0 to-reborn-white items-center">
-        <AppBar />
-        <main className="w-full h-1 flex-1 flex flex-row min-w-[1140px] max-w-[2440px] justify-center bg-reborn-white">
-          <div className="flex flex-shrink-0 h-full w-[243px]">
-            <SideBar />
-          </div>
-          <Outlet />
-        </main>
-      </div>
-    </ConfirmDialogProvider>
+    <StompProvider>
+      <ConfirmDialogProvider>
+        <ConfirmDialog />
+        <div className="w-full h-full flex flex-col bg-gradient-to-r from-reborn-gray0 to-reborn-white items-center">
+          <AppBar />
+          <main className="w-full h-1 flex-1 flex flex-row min-w-[1140px] max-w-[2440px] justify-center bg-reborn-white">
+            <div className="flex flex-shrink-0 h-full w-[243px]">
+              <SideBar />
+            </div>
+            <Outlet />
+          </main>
+        </div>
+      </ConfirmDialogProvider>
+    </StompProvider>
   );
 };
