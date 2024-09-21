b3764b47b4bc8992abfe44774596532b2c5e80d4
HanKyeon <hgh21233@gmail.com>
2024-09-22 01:16:56 +0900

    chore: 주석 추가

--- src/queries/chat/chat-domain.ts
+++ src/queries/chat/chat-domain.ts
@@ -1,25 +1,49 @@
 import { Domain } from 'src/api/endpoints';
 
 class ChatDomain {
+  /**
+   * @description get
+   */
   get chatList() {
     return Domain.getPath('/api/chatting/room/list');
   }
+  /**
+   * @description post
+   */
   get chatStart() {
     return Domain.getPath('/api/chatting/room');
   }
+  /**
+   * @description post
+   */
   sendMessage(roomId: number | string) {
     return Domain.getPath(`/api/chatting/${roomId}/message`);
   }
+  /**
+   * @description put
+   */
   read(roomId) {
     return Domain.getPath(`/api/chatting/${roomId}/message/read`);
   }
+  /**
+   * @description get
+   */
   getUnreadMessage(roomId: number | string) {
     return Domain.getPath(`/chatting/${roomId}/message/new`);
   }
+  /**
+   * @description get
+   */
   getAllMessage(roomId: string, pageNo: number) {
     return Domain.getPath(
       `/api/chatting/${roomId}/message/list/v2?pageNo=${pageNo}`,
     );
+  }
+  /**
+   * @description get
+   */
+  bookingDetail(userId: string) {
+    return Domain.getPath(`/api/booking/calendar/detail?userId=${userId}`);
   }
 }
 
