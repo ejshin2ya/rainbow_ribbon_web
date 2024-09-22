d8e093b41cc2036a98fee75de46e08c0d783fddf
HanKyeon <hgh21233@gmail.com>
2024-09-22 19:01:09 +0900

    feat: 알람 서비스 연결

--- src/components/alarm-popover/AlarmPopoverTabs.tsx
+++ src/components/alarm-popover/AlarmPopoverTabs.tsx
@@ -1,3 +1,5 @@
+import { useAlarmCount } from 'src/queries/alarm';
+
 interface Props {
   tab: '예약' | '채팅';
   setReservation: () => void;
@@ -10,6 +12,9 @@ export const AlarmPopoverTabs = function ({
   tab,
 }: Props) {
   const isChat = tab === '채팅';
+  const { data } = useAlarmCount();
+  const bookingCount = data?.data.booking ?? 0;
+  const chatCount = data?.data.chat ?? 0;
   return (
     <div className="w-full h-[50px] flex-shrink-0 flex flex-row relative">
       <div
@@ -21,9 +26,11 @@ export const AlarmPopoverTabs = function ({
         >
           예약
         </span>
-        <span className="bg-reborn-orange3 text-reborn-white rounded-full min-w-[16px] h-[16px] flex items-center justify-center font-medium text-[12px] leading-[18px] text-center p-[1px]">
-          3
-        </span>
+        {!!bookingCount && (
+          <span className="bg-reborn-orange3 text-reborn-white rounded-full min-w-[16px] h-[16px] flex items-center justify-center font-medium text-[12px] leading-[18px] text-center p-[1px]">
+            {bookingCount}
+          </span>
+        )}
       </div>
       <div
         className={`flex-1 flex items-center justify-center flex-row gap-[4px]`}
@@ -34,9 +41,11 @@ export const AlarmPopoverTabs = function ({
         >
           채팅
         </span>
-        <span className="bg-reborn-orange3 text-reborn-white rounded-full min-w-[16px] h-[16px] flex items-center justify-center font-medium text-[12px] leading-[18px] text-center p-[1px]">
-          224
-        </span>
+        {!!chatCount && (
+          <span className="bg-reborn-orange3 text-reborn-white rounded-full min-w-[16px] h-[16px] flex items-center justify-center font-medium text-[12px] leading-[18px] text-center p-[1px]">
+            {chatCount}
+          </span>
+        )}
       </div>
       <div
         className={`absolute bottom-0 right-0 w-full border-[1px] border-reborn-gray2`}
