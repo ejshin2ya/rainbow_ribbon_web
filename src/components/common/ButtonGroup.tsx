4dd6e695a074ee9215482429ba0fb564b22e9c18
HanKyeon <hgh21233@gmail.com>
2024-09-24 23:22:20 +0900

    feat: 버튼 그룹에 disabled값 추가

--- src/components/common/ButtonGroup.tsx
+++ src/components/common/ButtonGroup.tsx
@@ -1,7 +1,10 @@
+import { ReactNode } from 'react';
+
 interface ButtonOptions {
   onClick: () => void;
-  text?: string;
+  text?: string | ReactNode;
   className?: string;
+  disabled?: boolean;
 }
 interface Props {
   cancelButtonOptions?: ButtonOptions;
@@ -17,6 +20,7 @@ export const ButtonGroup = function ({
         <button
           onClick={cancelButtonOptions.onClick}
           className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] border-reborn-gray2 text-reborn-gray4 border-[1px] rounded-[4px] py-[5px] px-[10px] ${cancelButtonOptions.className}`}
+          disabled={cancelButtonOptions.disabled ?? false}
         >
           {cancelButtonOptions.text}
         </button>
@@ -25,6 +29,7 @@ export const ButtonGroup = function ({
         <button
           onClick={confirmButtonOptions.onClick}
           className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] text-reborn-white rounded-[4px] py-[5px] px-[10px] bg-reborn-orange3 ${confirmButtonOptions.className}`}
+          disabled={confirmButtonOptions.disabled ?? false}
         >
           {confirmButtonOptions.text}
         </button>
