39aecea0e8dd566101de5c3d1fe0650c7314ce5e
HanKyeon <hgh21233@gmail.com>
2024-10-03 03:04:27 +0900

    fix: 내부 로그인 로직 제거

--- src/queries/chat/StompComp.tsx
+++ src/queries/chat/StompComp.tsx
@@ -3,7 +3,8 @@ import { Client, Message } from '@stomp/stompjs';
 import { Domain } from 'src/api/endpoints';
 import { useQueryClient } from '@tanstack/react-query';
 import api from 'src/api/axios';
-import { useMutation } from '@tanstack/react-query';
+import { useAuth } from 'src/contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 interface StompContextProps {
   client: Client;
@@ -16,129 +17,108 @@ export const StompProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   // TODO: messages는 추후 삭제해야 함
   const [messages, setMessages] = useState<string[]>([]);
-  // TODO: 로그인 mutation 삭제해야 함
-  const { mutateAsync, isSuccess } = useMutation({
-    mutationFn: async () => {
-      return (
-        await api<{
-          statusCode: string;
-          msg: string;
-          data: {
-            accessToken: string;
-            refrechToken: string;
-            userType: string;
-            name: string;
-            phone: string;
-          };
-        }>({
-          method: 'post',
-          data: { loginId: 'woooriii@naver.com', password: 'woooriii' },
-          url: '/api/no-auth/account/company/login',
-        })
-      ).data;
-    },
-  });
+  const { accessToken } = useAuth();
   const [client, setClient] = useState<Client>();
 
   useEffect(() => {
+    if (!accessToken) return navigate('/login');
     let stompClient: Client;
     let heartbeatInterval;
-    mutateAsync().then(res => {
-      const socketUrl = Domain.getPath('/chatting');
-      const socket = new WebSocket(socketUrl);
 
-      api.defaults.headers.common.Authorization =
-        'Bearer ' + res.data.accessToken;
+    const socketUrl = Domain.getPath('/chatting');
+    const socket = new WebSocket(socketUrl);
 
-      stompClient = new Client({
-        webSocketFactory: () => socket,
-        // brokerURL: socketUrl,
-        debug: str => {
-          if (process.env.NODE_ENV === 'development')
-            console.log(`%cDebug\n${str}`, 'color: red;');
-        },
-        reconnectDelay: 10 * 1000,
-        heartbeatIncoming: 25 * 1000,
-        heartbeatOutgoing: 25 * 1000,
-        connectHeaders: {
-          Authorization: `Bearer ${res.data.accessToken}`,
-        },
-      });
+    api.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
 
-      stompClient.onConnect = frame => {
-        stompClient?.subscribe(
-          '/user/queue/notifications',
-          (message: Message) => {
-            console.log('유저 큐 노티 메세지', message);
-            if (message.body) {
-              setMessages(prevMessages => [...prevMessages, message.body]);
-              console.log('Received', message.body);
-              // TODO: 메세지에 따라 세세하게 refetch
-              queryClient.invalidateQueries({ queryKey: ['chat'] });
-            }
-          },
-          {
-            id: frame.headers?.['user-name'],
-            'user-name': frame.headers?.['user-name'],
-            version: frame.headers?.['version'],
-            'heart-beat': frame.headers?.['heart-beat'],
-            Authorization: `Bearer ${res.data.accessToken}`,
-          },
-        );
-
-        heartbeatInterval = setInterval(() => {
-          if (stompClient?.connected) {
-            stompClient.publish({
-              destination: '/heartbeat',
-              body: 'Heartbeat',
-            });
-          }
-        }, 25000);
-      };
-
-      stompClient.onWebSocketClose = e => {
-        if (process.env.NODE_ENV === 'development') {
-          console.warn('WebSocket closed with code:', e.code);
-          console.warn('WebSocket close reason:', e.reason);
-        }
-
-        switch (e.code) {
-          case 1000:
-            if (process.env.NODE_ENV === 'development')
-              console.log('WebSocket 정상적으로 종료되었습니다.');
-            break;
-          case 1006:
-            if (process.env.NODE_ENV === 'development')
-              console.error('WebSocket 비정상 종료. 재연결을 시도합니다.');
-            if (stompClient?.reconnectDelay) {
-              stompClient.activate(); // 재연결 시도
-            }
-            break;
-          default:
-            if (process.env.NODE_ENV === 'development')
-              console.error('WebSocket closed with an unhandled code:', e.code);
-        }
-      };
-
-      stompClient.onStompError = frame => {
-        if (process.env.NODE_ENV === 'development') {
-          console.error('STOMP error: ' + frame.headers['message']);
-          console.error('Error details: ' + frame.body);
-        }
-      };
-
-      stompClient.onWebSocketError = error => {
-        if (process.env.NODE_ENV === 'development') {
-          console.error('WebSocket error:', error);
-          console.log('readyState', stompClient?.webSocket?.readyState);
-        }
-      };
-
-      stompClient.activate();
-      setClient(stompClient);
+    stompClient = new Client({
+      webSocketFactory: () => socket,
+      // brokerURL: socketUrl,
+      debug: str => {
+        if (process.env.NODE_ENV === 'development')
+          console.log(`%cDebug\n${str}`, 'color: red;');
+      },
+      reconnectDelay: 10 * 1000,
+      heartbeatIncoming: 25 * 1000,
+      heartbeatOutgoing: 25 * 1000,
+      connectHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
+
+    stompClient.onConnect = frame => {
+      stompClient?.subscribe(
+        '/user/queue/notifications',
+        (message: Message) => {
+          console.log('유저 큐 노티 메세지', message);
+          if (message.body) {
+            setMessages(prevMessages => [...prevMessages, message.body]);
+            console.log('Received', message.body);
+            // TODO: 메세지에 따라 세세하게 refetch
+            queryClient.invalidateQueries({ queryKey: ['chat'] });
+          }
+        },
+        {
+          id: frame.headers?.['user-name'],
+          'user-name': frame.headers?.['user-name'],
+          version: frame.headers?.['version'],
+          'heart-beat': frame.headers?.['heart-beat'],
+          Authorization: `Bearer ${accessToken}`,
+        },
+      );
+
+      heartbeatInterval = setInterval(() => {
+        if (stompClient?.connected) {
+          stompClient.publish({
+            destination: '/heartbeat',
+            body: 'Heartbeat',
+          });
+        }
+      }, 25000);
+    };
+
+    stompClient.onWebSocketClose = e => {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn('WebSocket closed with code:', e.code);
+        console.warn('WebSocket close reason:', e.reason);
+      }
+
+      switch (e.code) {
+        case 1000:
+          if (process.env.NODE_ENV === 'development')
+            console.log('WebSocket 정상적으로 종료되었습니다.');
+          break;
+        case 1006:
+          if (process.env.NODE_ENV === 'development')
+            console.error('WebSocket 비정상 종료. 재연결을 시도합니다.');
+          if (stompClient?.reconnectDelay) {
+            stompClient.activate(); // 재연결 시도
+          }
+          break;
+        default:
+          if (process.env.NODE_ENV === 'development')
+            console.error('WebSocket closed with an unhandled code:', e.code);
+      }
+    };
+
+    stompClient.onStompError = frame => {
+      if (process.env.NODE_ENV === 'development') {
+        console.error('STOMP error: ' + frame.headers['message']);
+        console.error('Error details: ' + frame.body);
+      }
+    };
+
+    stompClient.onWebSocketError = error => {
+      if (process.env.NODE_ENV === 'development') {
+        console.error('WebSocket error:', error);
+        console.log('readyState', stompClient?.webSocket?.readyState);
+      }
+    };
+
+    stompClient.activate();
+    setClient(stompClient);
 
     return () => {
       if (stompClient?.connected) {
@@ -150,8 +130,6 @@ export const StompProvider: React.FC<{ children: React.ReactNode }> = ({
     };
   }, []);
 
-  // TODO: 로그인 뮤테이션 빼면 지워줘야함
-  if (!isSuccess) return null;
   if (!client) return null;
 
   return (
