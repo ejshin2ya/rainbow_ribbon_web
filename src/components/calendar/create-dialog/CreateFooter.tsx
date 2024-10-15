8a16fbae4bcd99ae5f59a14981887c220fb38f25
HanKyeon <hgh21233@gmail.com>
2024-10-15 21:17:39 +0900

    feat: 생성 dialog submit button formState를 이용하도록 변경

--- src/components/calendar/create-dialog/CreateFooter.tsx
+++ src/components/calendar/create-dialog/CreateFooter.tsx
@@ -1,9 +1,13 @@
+import { useFormContext } from 'react-hook-form';
+
 export const CreateFooter = function () {
+  const { formState } = useFormContext();
   return (
-    <footer className="w-full h-[120px] p-[30px] items-center justify-center flex-shrink-0 border-t-[1px] border-t-reborn-gray0">
+    <footer className="w-full h-[120px] p-[30px] items-center justify-center flex-shrink-0 border-t-[1px] border-t-reborn-gray1">
       <button
         type="submit"
-        className={`w-full h-full rounded-[8px] font-bold text-[18px] bg-reborn-gray1 text-reborn-gray3`}
+        className={`w-full h-full rounded-[8px] font-bold text-[18px] ${formState.isDirty ? 'bg-reborn-orange3 text-reborn-white' : 'bg-reborn-gray1 text-reborn-gray3'}`}
+        disabled={!formState.isDirty}
       >
         완료
       </button>
