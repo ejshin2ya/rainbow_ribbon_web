c3e5e5acc5853bf75a2d27949211f61963f9ae34
HanKyeon <hgh21233@gmail.com>
2024-10-18 01:00:47 +0900

    feat: dropdown 구현

--- src/components/calendar/create-dialog/FormSelect.tsx
+++ src/components/calendar/create-dialog/FormSelect.tsx
@@ -1,4 +1,4 @@
-import { SelectHTMLAttributes } from 'react';
+import { SelectHTMLAttributes, useEffect, useRef, useState } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import { ReactComponent as ArrowDownIcon } from 'src/assets/ArrowDown.svg';
 
@@ -11,35 +11,59 @@ interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
 
 export const FormSelect = function ({
   name,
-  optionList,
+  optionList = [],
   placeHolder,
   className,
   ...rest
 }: Props) {
+  const dropdownRef = useRef<HTMLUListElement>(null);
+  const [dropdown, setDropdown] = useState(false);
   const { control } = useFormContext();
   const { field } = useController({ name, control });
-
+  const toggleDropdown = function () {
+    setDropdown(prev => !prev);
+  };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropdown(false); // 드롭다운 외부를 클릭하면 닫기
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdown]);
   return (
     <div
-      className={`relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 ${className ?? ''}`}
+      className={`relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 cursor-pointer hover:bg-reborn-gray0 active:bg-reborn-gray1 duration-200 ${className ?? ''} ${field.value ? 'text-reborn-gray8' : 'text-reborn-gray3'}`}
+      onClick={toggleDropdown}
     >
-      <select
-        className="w-full h-full outline-none appearance-none text-[14px]"
-        defaultValue={''}
-        {...rest}
-        {...field}
-      >
-        <option value="" disabled hidden>
-          {placeHolder}
-        </option>
-        {optionList?.map((option, idx) => {
-          return (
-            <option key={`${name}-opt-${idx}`} value={option.value}>
+      <div>{field.value || placeHolder}</div>
+      {!!optionList.length && dropdown && (
+        <ul
+          className="absolute mt-[8px] left-0 w-full bg-white border border-gray-300 rounded-md shadow-lg z-[1422] max-h-[170px] overflow-y-auto animate-scaleUp"
+          onClick={e => {
+            e.stopPropagation();
+            field.onChange(`${(e.target as HTMLLIElement).value}`);
+            setDropdown(false);
+          }}
+          ref={dropdownRef}
+        >
+          {optionList.map((option, idx) => (
+            <li
+              key={`${name}-${option.value}-${idx}`}
+              className="px-3 py-2 cursor-pointer hover:bg-gray-100 duration-200 text-reborn-gray8"
+              value={option.value}
+            >
               {option.title}
-            </option>
-          );
-        })}
-      </select>
+            </li>
+          ))}
+        </ul>
+      )}
       <label className="absolute inset-y-0 right-[12px] flex items-center px-2 pointer-events-none">
         <ArrowDownIcon style={{ color: '#858583' }} />
       </label>
