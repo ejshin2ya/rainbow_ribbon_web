984b902f42d515a08a822aa64d01ef6027d8e609
HanKyeon <hgh21233@gmail.com>
2024-10-17 23:21:24 +0900

    feat: 버튼 관련 valid 적용 및 hover, activate 적용

--- src/components/calendar/create-dialog/CreateFooter.tsx
+++ src/components/calendar/create-dialog/CreateFooter.tsx
@@ -6,8 +6,8 @@ export const CreateFooter = function () {
     <footer className="w-full h-[120px] p-[30px] items-center justify-center flex-shrink-0 border-t-[1px] border-t-reborn-gray1">
       <button
         type="submit"
-        className={`w-full h-full rounded-[8px] font-bold text-[18px] duration-200 ${formState.isDirty ? 'bg-reborn-orange3 text-reborn-white' : 'bg-reborn-gray1 text-reborn-gray3'}`}
-        disabled={!formState.isDirty}
+        className={`w-full h-full rounded-[8px] font-bold text-[18px] duration-200 ${formState.isValid ? 'bg-reborn-orange3 text-reborn-white active:bg-reborn-orange2_4 hover:bg-reborn-orange2_3' : 'bg-reborn-gray1 text-reborn-gray3'}`}
+        disabled={!formState.isValid}
       >
         완료
       </button>
