49029bd8981535ce1be9aeab252e5cfeeb23a1bc
HanKyeon <hgh21233@gmail.com>
2024-10-18 01:00:39 +0900

    feat: 버튼 그룹 인터랙션 추가

--- src/components/common/ButtonGroup.tsx
+++ src/components/common/ButtonGroup.tsx
@@ -31,7 +31,7 @@ export const ButtonGroup = function ({
             await cancelButtonOptions?.onClick?.();
             await afterCancelCallback?.();
           }}
-          className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] border-reborn-gray2 text-reborn-gray4 border-[1px] rounded-[4px] py-[5px] px-[10px] ${cancelButtonOptions.className}`}
+          className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] border-reborn-gray2 text-reborn-gray4 border-[1px] rounded-[4px] py-[5px] px-[10px] hover:bg-reborn-gray0 active:bg-reborn-gray1 duration-200 ${cancelButtonOptions.className}`}
           disabled={cancelButtonOptions.disabled ?? false}
         >
           {cancelButtonOptions.text}
@@ -44,7 +44,7 @@ export const ButtonGroup = function ({
             await confirmButtonOptions?.onClick?.();
             await afterConfirmCallback?.();
           }}
-          className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] text-reborn-white rounded-[4px] py-[5px] px-[10px] bg-reborn-orange3 ${confirmButtonOptions.className}`}
+          className={`flex-1 h-full flex items-center justify-center font-medium text-[14px] leading-[21px] text-reborn-white rounded-[4px] py-[5px] px-[10px] bg-reborn-orange3 hover:bg-reborn-orange2_3 active:bg-reborn-orange2_4 duration-200 ${confirmButtonOptions.className}`}
           disabled={confirmButtonOptions.disabled ?? false}
         >
           {confirmButtonOptions.text}
