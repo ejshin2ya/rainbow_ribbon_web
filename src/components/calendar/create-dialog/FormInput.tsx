f3af0e8e895a3de09768f6bfecd7f21b5fd6050f
HanKyeon <hgh21233@gmail.com>
2024-10-17 23:22:50 +0900

    feat: hook-form에 쓰일 input, select, textarea component 추가 및 기본 props 전개

--- src/components/calendar/create-dialog/FormInput.tsx
+++ src/components/calendar/create-dialog/FormInput.tsx
@@ -1,17 +1,25 @@
+import { InputHTMLAttributes } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeHolder?: string;
+  className?: string;
 }
 
-export const FormInput = function ({ name, placeHolder }: Props) {
+export const FormInput = function ({
+  name,
+  placeHolder,
+  className,
+  ...rest
+}: Props) {
   const { control } = useFormContext();
   const { field } = useController({ name, control });
   return (
     <input
-      className="relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 outline-none text-[14px]"
+      className={`no-input-spinner relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 outline-none text-[14px] ${className ?? ''}`}
       placeholder={placeHolder}
+      {...rest}
       {...field}
     />
   );
--- src/components/calendar/create-dialog/FormSelect.tsx
+++ src/components/calendar/create-dialog/FormSelect.tsx
@@ -1,28 +1,44 @@
+import { SelectHTMLAttributes } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import { ReactComponent as ArrowDownIcon } from 'src/assets/ArrowDown.svg';
 
-interface Props {
+interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
   name: string;
   placeHolder?: string;
-  optionList?: string;
+  optionList?: { value: string; title: string }[];
+  className?: string;
 }
 
-export const FormSelect = function ({ name, optionList, placeHolder }: Props) {
+export const FormSelect = function ({
+  name,
+  optionList,
+  placeHolder,
+  className,
+  ...rest
+}: Props) {
   const { control } = useFormContext();
   const { field } = useController({ name, control });
 
   return (
-    <div className="relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1">
+    <div
+      className={`relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 ${className ?? ''}`}
+    >
       <select
         className="w-full h-full outline-none appearance-none text-[14px]"
-        {...field}
         defaultValue={''}
+        {...rest}
+        {...field}
       >
         <option value="" disabled hidden>
           {placeHolder}
         </option>
-        <option value={'ㅎㅇ1'}>ㅎㅇ1</option>
-        <option value={'ㅎㅇ2'}>ㅎㅇ2</option>
+        {optionList?.map((option, idx) => {
+          return (
+            <option key={`${name}-opt-${idx}`} value={option.value}>
+              {option.title}
+            </option>
+          );
+        })}
       </select>
       <label className="absolute inset-y-0 right-[12px] flex items-center px-2 pointer-events-none">
         <ArrowDownIcon style={{ color: '#858583' }} />
