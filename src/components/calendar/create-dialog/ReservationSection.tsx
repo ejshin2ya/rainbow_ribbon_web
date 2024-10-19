2918bfd1c39cc049e17785ef36ee975da2bf1663
HanKyeon <hgh21233@gmail.com>
2024-10-19 17:33:45 +0900

    feat: package options 추가

--- src/components/calendar/create-dialog/ReservationSection.tsx
+++ src/components/calendar/create-dialog/ReservationSection.tsx
@@ -1,10 +1,23 @@
 import { useFormContext } from 'react-hook-form';
 import { FormSelect } from './FormSelect';
+import { useQuery } from '@tanstack/react-query';
+import { getCompanyInfo } from 'src/services/companyService';
+import { useFuneralOptions } from 'src/queries/reservation';
 
 export const ReservationSection = function () {
   const { watch } = useFormContext();
   const start = watch('bookingStart') || `0`;
   const end = watch('bookingEnd') || `0.1`;
+  // TODO: 쿼리문 연동 및 삭제
+  const { data } = useQuery({
+    queryKey: ['company'],
+    queryFn: () => {
+      return getCompanyInfo().then(res => {
+        return res;
+      });
+    },
+  });
+  const { data: funeralOptions } = useFuneralOptions(data?.data.id ?? '');
 
   const timeArray = new Array(25).fill(0).map((_, idx) => {
     return {
@@ -21,9 +34,24 @@ export const ReservationSection = function () {
       <div className="w-full flex-1 h-1 flex flex-col gap-[6px]">
         <h2 className="text-[14px]">패키지</h2>
 
-        <FormSelect name="package" placeHolder="패키지 선택" />
+        <FormSelect
+          name="package"
+          placeHolder="패키지 선택"
+          optionList={[
+            { title: '단순 패키지', value: '단순' },
+            { title: '기본 패키지', value: '기본' },
+            { title: '보통 패키지', value: '보통' },
+          ]}
+        />
 
-        <FormSelect name="option" placeHolder="옵션 선택" />
+        <FormSelect
+          name="option"
+          placeHolder="옵션 선택"
+          optionList={funeralOptions?.data.data.map(fun => ({
+            title: fun.funeralType,
+            value: fun.funeralId,
+          }))}
+        />
 
         <h2 className="text-[14px] mt-[6px]">예약 시간</h2>
         <div className="w-full h-[46px] flex felx-row gap-[10px]">
