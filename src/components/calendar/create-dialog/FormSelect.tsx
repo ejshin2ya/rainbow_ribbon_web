2692048787f0986b8d9a5b11c07c97797d061a66
HanKyeon <hgh21233@gmail.com>
2024-10-19 17:34:04 +0900

    feat: 이벤트 위임 이슈 수정

--- src/components/calendar/create-dialog/FormSelect.tsx
+++ src/components/calendar/create-dialog/FormSelect.tsx
@@ -36,6 +36,7 @@ export const FormSelect = function ({
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [dropdown]);
+
   return (
     <div
       className={`relative w-full h-[46px] border-[1px] py-[12.5px] px-[16px] rounded-[8px] border-reborn-gray1 cursor-pointer hover:bg-reborn-gray0 active:bg-reborn-gray1 duration-200 ${className ?? ''} ${field.value ? 'text-reborn-gray8' : 'text-reborn-gray3'}`}
@@ -48,22 +49,20 @@ export const FormSelect = function ({
       {!!optionList.length && dropdown && (
         <ul
           className="absolute mt-[8px] left-0 w-full bg-white border border-gray-300 rounded-md shadow-lg z-[1422] max-h-[170px] overflow-y-auto animate-scaleUp"
-          onClick={e => {
-            e.stopPropagation();
-            field.onChange(`${(e.target as HTMLLIElement).value}`);
-            setDropdown(false);
-          }}
           ref={dropdownRef}
         >
-          {optionList.map((option, idx) => (
-            <li
-              key={`${name}-${option.value}-${idx}`}
-              className="px-3 py-2 cursor-pointer hover:bg-gray-100 duration-200 text-reborn-gray8"
-              value={option.value}
-            >
-              {option.title}
-            </li>
-          ))}
+          {optionList.map((option, idx) => {
+            return (
+              <li
+                key={`${name}-${option.value}-${idx}`}
+                className="px-3 py-2 cursor-pointer hover:bg-gray-100 duration-200 text-reborn-gray8"
+                // value={`${option.value}`}
+                onClick={() => field.onChange(`${option.value}`)}
+              >
+                {option.title}
+              </li>
+            );
+          })}
         </ul>
       )}
       <label className="absolute inset-y-0 right-[12px] flex items-center px-2 pointer-events-none">
